26a179542719f286d12cc949add31f75726a08eb
CoderJap <japjotwork007@gmail.com>
2024-01-29 22:41:54 +0530

    adding date objects program in js codes

---
+++ Concepts/date.js
@@ -0,0 +1,16 @@
+// Date(year, month, day, hour, minute, second, ms)
+const date = new Date();
+console.log(date);
+
+// extraction
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
