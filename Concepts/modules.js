92fbe0f1a6a5fdeeb4efc423795882906bc5112a
CoderJap <japjotwork007@gmail.com>
2024-01-31 21:48:34 +0530

    adding some programs in js codes

--- Concepts/modules.js
+++ Concepts/modules.js
@@ -4,8 +4,15 @@
 //                Can conatin variables, classes, functions.... and more
 //                Introduced as part of ECMAScript 2015 update
 
-// make sure to choose type="module" in script tag when u need to import module into any index.js after that write the below code 
+//  ** make sure to choose type="module" in script tag when u need to import module into any index.js after that write the below code 
 
 import {PI, getCircumference, getArea } from '../Concepts/module#1.js';
 
 console.log(PI);
+const circumference = getCircumference(10);
+
+console.log(`${Circumference.toFixed(2)}cm`);
+
+
+// this code currently gives error since we havent yet included type="module" i the script tag as we are
+// currently only writing js code but still i have written the syntax to know how to import/export modules for future reference 
